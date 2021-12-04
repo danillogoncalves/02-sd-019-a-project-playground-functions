589056c19c5837185c81d983617e6bcba033809f
danillo <danillogonvalves001@gmail.com>
2021-12-04 13:32:59 -0300

    Décimo primeiro exercíco feito.

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,8 +18,29 @@ function techList(array, string) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
   // seu código aqui
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  //  let comparison =array[0]
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] < 0 || array[i] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    let count = 0
+    for (let s = 0; s < array.length; s += 1) {
+      if (array[i] === array[s]) {
+        count += 1;
+      }
+      if (count > 2) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+  }
+  return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;;
 }
 
 // Desafio 12
